9e8c5f42961b7ff7cbd70b5f2101cb34548a5d78
Vivek <vivekaxl@gmail.com>
2017-04-11 00:41:30 -0400

    reduced dimensions

--- Data/pca.py
+++ Data/pca.py
@@ -8,11 +8,22 @@ import matplotlib.pyplot as plt
 files = ["TriMesh_1_2.csv", "TriMesh_2_3.csv", "SaC1_2.csv", "SaC_11_12.csv", "SaC_3_4.csv", "SaC_5_6.csv", "SaC_7_8.csv", "SaC_9_10.csv", "x264-DB_1_2.csv", "x264-DB_2_3.csv", "x264-DB_3_4.csv", "x264-DB_4_5.csv", "x264-DB_5_6.csv",]
 
 for file in files:
+    print file
     data = pd.read_csv(file)
+    columns = data.columns
     indep_columns = [c for c in data.columns if "<$" not in c]
+    dep_columns = [c for c in data.columns if '<$' in c]
     pca = PCA(n_components=9)
     X_reduced = pca.fit_transform(data[indep_columns])
-    np.savetxt('reduced_' + file, X_reduced, delimiter=',')
+    X = pd.DataFrame(X_reduced)
+    # print len(X.columns), len(data[dep_columns[0]]), len(data[dep_columns[1]]), len(data)
+    X['<$1'] = data[dep_columns[0]]
+    X['<$2'] = data[dep_columns[1]]
+    # print len(X.columns), len(columns)
+    column_names = ['$'+str(i+1) for i in xrange(9)]
+    X.columns = column_names + ['<$1', '<$2']
+
+    X.to_csv('reduced_'+file, index=False)
 
 
 
