22c04957c7da3bb38842d1885b51076a68b7d323
Vivek <vivekaxl@gmail.com>
2017-04-15 19:54:09 -0400

    removing .p files

--- runner.py
+++ runner.py
@@ -1,9 +1,10 @@
 import os
 
-files = [f for f in os.listdir(".") if "al1-" in f]
-files += [f for f in os.listdir(".") if "al2-" in f]
-files += [f for f in os.listdir(".") if "mmre-" in f]
-files += [f for f in os.listdir(".") if "rank-" in f]
+files = []
+files += [f for f in os.listdir(".") if "al1-" in f and ".py" in f]
+files += [f for f in os.listdir(".") if "al2-" in f and ".py" in f]
+files += [f for f in os.listdir(".") if "mmre-" in f and ".py" in f]
+files += [f for f in os.listdir(".") if "rank-" in f and ".py" in f]
 
 
 for file in files:
