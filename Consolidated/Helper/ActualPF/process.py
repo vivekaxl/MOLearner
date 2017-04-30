6a990162b11b9ee124bd4bcc22f731298436562e
Vivek Nair <vivekaxl@gmail.com>
2017-04-30 02:59:19 -0400

    added new helper code

--- Consolidated/Helper/ActualPF/process.py
+++ Consolidated/Helper/ActualPF/process.py
@@ -10,15 +10,17 @@ import os
 
 
 consolidated_dict = {}
-files = ["actual_pf_SaC_9_10.p", "actual_pf_xomoo2-p10000-d27-o4-dataset3.p"]
+files = [f for f in os.listdir(".") if ".p" in f and ".py" not in f]
 for i, file in enumerate(files):
     print file
     content = pickle.load(open(file, 'r'))
     for key in sorted(content.keys()):
         consolidated_dict[key] = content[key]
         print key, len(consolidated_dict.keys())
-all_files = [f for f in os.listdir(".") if ".py" not in f and "consolidated" not in f]
+all_files = [f for f in os.listdir(".") if ".py" not in f and "consolidated" not in f and ".md" not in f]
 for i, f in enumerate(all_files):
     print i, f
+
+print len(all_files), len(consolidated_dict.keys())
 assert(len(all_files) == len(consolidated_dict.keys())), "Something is wrong"
 pickle.dump(consolidated_dict, open("consolidated_dict.p", 'w'))
--- Consolidated/Helper/get_pf.py
+++ Consolidated/Helper/get_pf.py
@@ -2,6 +2,7 @@ from __future__ import division
 import numpy as np
 import os
 import sys
+
 from utility import lessismore
 from utility import draw_pareto_front, generational_distance, ranges,inverted_generational_distance
 import pickle
@@ -9,41 +10,33 @@ import pickle
 if __name__ == "__main__":
     from utility import read_file, split_data, build_model
     from non_dominated_sort import non_dominated_sort
-    # files = ["./Data/" + f for f in os.listdir("./Data/") if ".csv" in f]
 
     files = [
-        "./Data/sort_256.csv",
-        "./Data/TriMesh_1_2.csv",
-        "./Data/TriMesh_2_3.csv",
-        "./Data/wc+rs-3d-c4.csv",
-        "./Data/wc+sol-3d-c4.csv",
-        "./Data/wc+wc-3d-c4.csv",
-        "./Data/wc-3d-c4.csv",
-        "./Data/wc-5d-c5.csv",
-        "./Data/wc-6d-c1.csv",
-        "./Data/wc-c1-3d-c1.csv",
-        "./Data/wc-c3-3d-c1.csv",
-        "./Data/x264-DB_1_2.csv",
-        "./Data/x264-DB_2_3.csv",
-        "./Data/x264-DB_3_4.csv",
-        "./Data/x264-DB_4_5.csv",
-        "./Data/x264-DB_5_6.csv",
-        "./Data/xomo_all-p10000-d27-o4-dataset1.csv",
-        "./Data/xomo_all-p10000-d27-o4-dataset2.csv",
-        "./Data/xomo_all-p10000-d27-o4-dataset3.csv",
-        "./Data/xomo_flight-p10000-d27-o4-dataset1.csv",
-        "./Data/xomo_flight-p10000-d27-o4-dataset2.csv",
-        "./Data/xomo_flight-p10000-d27-o4-dataset3.csv",
-        "./Data/xomo_ground-p10000-d27-o4-dataset1.csv",
-        "./Data/xomo_ground-p10000-d27-o4-dataset2.csv",
-        "./Data/xomo_ground-p10000-d27-o4-dataset3.csv",
-        "./Data/xomo_osp-p10000-d27-o4-dataset1.csv",
-        "./Data/xomo_osp-p10000-d27-o4-dataset2.csv",
-        "./Data/xomo_osp-p10000-d27-o4-dataset3.csv",
-        "./Data/xomoo2-p10000-d27-o4-dataset1.csv",
-        "./Data/xomoo2-p10000-d27-o4-dataset2.csv",
-        "./Data/xomoo2-p10000-d27-o4-dataset3.csv",
-
+        '../../Data/llvm_input.csv',
+        '../../Data/noc_CM_log.csv',
+        '../../Data/sort_256.csv',
+        '../../Data/wc+rs-3d-c4.csv',
+        '../../Data/wc+sol-3d-c4.csv',
+        '../../Data/wc+wc-3d-c4.csv',
+        '../../Data/wc-3d-c4.csv',
+        '../../Data/wc-5d-c5.csv',
+        '../../Data/wc-6d-c1.csv',
+        '../../Data/wc-c1-3d-c1.csv',
+        '../../Data/wc-c3-3d-c1.csv',
+        '../../Data/rs-6d-c3.csv',
+        '../../Data/MONRP_50_4_5_0_110.csv',
+        '../../Data/MONRP_50_4_5_0_90.csv',
+        '../../Data/MONRP_50_4_5_4_110.csv',
+        '../../Data/MONRP_50_4_5_4_90.csv',
+        '../../Data/POM3A.csv',
+        '../../Data/POM3B.csv',
+        '../../Data/POM3C.csv',
+        '../../Data/POM3D.csv',
+        '../../Data/xomo_all.csv',
+        '../../Data/xomo_flight.csv',
+        '../../Data/xomo_ground.csv',
+        '../../Data/xomo_osp.csv',
+        '../../Data/xomoo2.csv'
     ]
 
     all_data = {}
@@ -61,4 +54,4 @@ if __name__ == "__main__":
         print len(true_pf)
 
         all_data[file]['pf'] = true_pf
-        pickle.dump(all_data, open('./Actual_PF/actual_pf_' + file.split('/')[-1][:-4] + '.p', 'w'))
+        pickle.dump(all_data, open('./ActualPF/actual_pf_' + file.split('/')[-1][:-4] + '.p', 'w'))
